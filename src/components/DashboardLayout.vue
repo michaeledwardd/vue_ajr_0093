98ff69531e9349b72c50c142b5085323e791cdf3
user <michedwards0@gmail.com>
2022-04-05 17:12:00 +0700

    first commit

---
+++ src/components/DashboardLayout.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="dashboard">
+    <v-navigation-drawer v-model="drawer" class="fullheight" width="256" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Michael Edward S </v-list-item-title>
+          <v-list-item-subtitle class="subtitle"> 190710093 </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          tag="router-link"
+          :to="item.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
+    <v-app-bar app fixed height="75px">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <VSpacer />
+      <v-toolbar-items>
+        <v-btn text router @click = "btnLogout"><v-icon>mdi-power</v-icon></v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <div class="fullheigh pa-5">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Dashboard", to: "/dashboard" },
+        { title: "Promo", to:"/promo" },
+        { title: "DetailShift", to:"/detailshift"},
+      ],
+    };
+  },
+  methods: {
+    btnLogout() {
+      localStorage.removeItem('id');
+      localStorage.removeItem('token');
+      location.reload();
+    },
+  }
+};
+</script>
+<style scoped>
+.fullheight {
+  min-height: 100vh !important;
+}
+.router{
+  text-decoration: none;
+  color: black;
+}
+.dashboard{
+  color:blue;
+}
+</style>
---
+++ src/components/DataMaster/DetailShift.vue
@@ -0,0 +1,268 @@
+<template>
+  <v-main class="list">
+    <h3 class="text-h3 font-weight-medium mb-5">Jadwal Kerja Pegawai AJR</h3>
+    
+    <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+
+        <v-spacer></v-spacer>
+
+        <v-btn color="success" dark @click="dialog = true"> Tambah </v-btn>
+
+      </v-card-title>
+      <v-data-table :headers="headers" :items="courses" :search="search">
+
+        <template v-slot:[`item.actions`]="{item}">
+                <v-btn icon small class="mr-2" @click="editHandler(item)">
+                  <v-icon color="red">mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn icon small @click="deleteHandler(item.id)">
+                     <v-icon color="green">mdi-delete</v-icon>
+                </v-btn>
+            </template>
+
+        <!-- <template v-slot:[`item.actions`]="{ item }">
+          <v-btn small class="mr-2" @click="editHandler(item)"> edit </v-btn>
+          <v-btn small @click="deleteHandler(item.id)"> delete </v-btn>
+        </template> -->
+      </v-data-table>
+    </v-card>
+    
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{formTitle}} Course</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-text-field v-model="form.nama_kelas" label="Nama Kelas" required></v-text-field>
+            <v-text-field v-model="form.kode" label="Kode" required></v-text-field>
+            <v-text-field v-model="form.biaya_pendaftaran" label="Biaya Pendaftaran" required></v-text-field>
+            <v-text-field v-model="form.kapasitas" label="Kapasitas" required></v-text-field>
+            
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="cancel"> Cancel </v-btn>
+          <v-btn color="blue darken-1" text @click="setForm"> Save </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+    <v-dialog v-model="dialogConfirm" persistent max-width="400px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">warning!</span>
+        </v-card-title>
+        <v-card-text> Anda yakin ingin menghapus kelas ini? </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialogConfirm = false">
+            Cancel
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="deleteData"> Delete </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+       <v-snackbar v-model="snackbar" :color="color" timeout="2000" bottom>{{ error_message }} </v-snackbar>
+
+  </v-main>
+</template>
+
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      inputType: 'Tambah',
+      load: false,
+      snackbar: false,
+      error_message: '',
+      color: '',
+      search: null,
+      dialog: false,
+      dialogConfirm: false,
+      headers: [
+        { text: "Hari Kerja", align: "start", sortable: true, value: "hari_kerja"},
+        { text: "Jenis Shift", value: 'jenis_shift'},
+        { text: "Nama Pegawai", value: 'nama_pegawai'},
+        // { text: "Keterangan", value: 'keterangan'},
+        // { text: "Status Promo", value: 'status_promo'},
+        { text: "Action", value:'actions'},
+      ],
+      course: new FormData,
+      courses: [],
+      form:{
+        nama_kelas: null,
+        kode: null,
+        biaya_pendaftaran: null,
+        kapasitas: null,
+      },
+      deleteId: '',
+      editId: ''
+    };
+  },
+
+  methods: {
+    setForm(){
+      if(this.inputType !== 'Tambah'){
+        this.update();
+      }
+      else{
+        this.save();
+      }
+    },
+
+    readData(){
+      var url = this.$api + '/detailshift';
+      this.$http.get(url, {
+        headers: {
+          'Authorization' : 'Bearer ' + localStorage.getItem('token')
+        }
+      }).then(response => {
+        this.courses = response.data.data;
+      })
+    },
+
+    save(){
+      this.course.append('nama_kelas',this.form.nama_kelas);
+      this.course.append('kode',this.form.kode);
+      this.course.append('biaya_pendaftaran',this.form.biaya_pendaftaran);
+      this.course.append('kapasitas', this.form.kapasitas);
+
+      var url= this.$api + '/course/'
+      this.load = true;
+      this.$http.post(url, this.course, {
+        headers: {
+          'Authorization' : 'Bearer ' + localStorage.getItem('token'),
+        }
+      }).then(response => {
+        this.error_message = response.data.message;
+        this.color = "green";
+        this.snackbar = true;
+        this.load = true;
+        this.close();
+        this.readData();
+        this.resetForm();
+      }).catch(error => {
+        this.error_message = error.response.data.message;
+        this.color = "red";
+        this.snackbar = true;
+        this.load = false;
+      });
+    },
+
+    update(){
+      let newData = {
+        nama_kelas : this.form.nama_kelas,
+        kode : this.form.kode,
+        biaya_pendaftaran : this.form.biaya_pendaftaran,
+        kapasitas: this.form.kapasitas
+      };
+      var url = this.$api + '/course/' + this.editId;
+      this.load = true;
+      this.$http.put(url, newData, {
+        headers: {
+          'Authorization' : 'Bearer ' + localStorage.getItem('token')
+        }
+      }).then(response => {
+        this.error_message = response.data.message;
+        this.color = 'green';
+        this.snackbar = true;
+        this.load = false;
+        this.close();
+        this.readData();
+        this.resetForm();
+        this.inputType = 'Tambah';
+      }).catch(error => {
+        this.error_message = error.response.data.message;
+        this.color = 'red';
+        this.snackbar = true;
+        this.load = false;
+      });
+    },
+
+    deleteData() {
+      //mengahapus data
+      var url = this.$api + '/course/' + this.deleteId;
+      //data dihapus berdasarkan id
+      this.$http.delete(url, {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token'),
+          },
+        })
+        .then((response) => {
+          this.error_message = response.data.message;
+          this.color = "green";
+          this.snackbar = true;
+          this.load = false;
+          this.close();
+          this.readData(); //mengambil data
+          this.resetForm();
+          this.inputType = "Tambah";
+        })
+        .catch((error) => {
+          this.error_message = error.response.data.message;
+          this.color = "red";
+          this.snackbar = true;
+          this.load = false;
+        });
+    },
+
+    editHandler(item){
+      this.inputType = 'Ubah';
+      this.editId = item.id;
+      this.form.nama_kelas = item.nama_kelas;
+      this.form.kode = item.kode;
+      this.form.biaya_pendaftaran = item.biaya_pendaftaran;
+      this.form.kapasitas = item.kapasitas;
+      this.dialog = true;
+    },
+
+    deleteHandler(id) {
+      this.deleteId = id;
+      this.dialogConfirm = true;
+    },
+    close() {
+      this.dialog = false;
+      this.inputType = "Tambah";
+      this.dialogConfirm = false;
+      this.readData();
+    },
+    cancel() {
+      this.resetForm();
+      this.readData();
+      this.dialog = false;
+      this.dialogConfirm = false;
+      this.inputType = "Tambah";
+    },
+    resetForm() {
+      this.form = {
+        nama_kelas: null,
+        kode: null,
+        biaya_pendaftaran: null,
+        kapasitas: null,
+      };
+    },
+  },
+  computed: {
+    formTitle() {
+      return this.inputType;
+    },
+  },
+  mounted() {
+    this.readData();
+  },
+};
+</script>
